03febfd4653fbcd2a909cd17705f376a8262d00c
well-in-that-case <ryanmoople@gmail.com>
2022-06-24 20:30:41 -0400

    Refactor test and benchmark file structure, see comment.

--- tests/basic.lua
+++ tests/basic.lua
@@ -1,3 +1,4 @@
+print "Testing compound assignment."
 a, b = 1, 2
 a += 1
 b += 1
@@ -69,3 +70,20 @@ a >>= 1
 b >>= 1
 assert(a == 0)
 assert(b == 1)
+
+print "Testing table length cache."
+local t = {}
+for i = 1, 1000 do
+    t[i] = "abc"
+end
+assert(#t == 1000, #t)
+
+for i = 1, 100 do
+    table.remove(t, i + math.random(1, 5))
+end
+assert(#t == 900, #t)
+
+local metatest = setmetatable({}, { __len = function () return 5 end })
+assert(#metatest == 5, #metatest)
+
+print "Finished."
