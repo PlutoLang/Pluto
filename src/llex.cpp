58ba58f1172513ede1577e3241507736861d9215
well-in-that-case <ryanmoople@gmail.com>
2022-07-26 22:02:08 -0400

    Allow warning suppression via comment attribute [[nowarn]].

--- src/llex.cpp
+++ src/llex.cpp
@@ -221,7 +221,6 @@ void luaX_setinput (lua_State *L, LexState *ls, ZIO *z, TString *source,
                     int firstchar) {
   ls->t.token = 0;
   ls->lasttoken = 0;
-  ls->laststat = TK_EOS;
   ls->L = L;
   ls->current = firstchar;
   ls->lookahead.token = TK_EOS;  /* no look-ahead token */
