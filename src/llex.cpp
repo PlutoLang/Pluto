720507a25a65e2b529b34511caafe60782a5d5c1
Sainan <63328889+Sainan@users.noreply.github.com>
2022-08-12 20:26:38 +0200

    Fix lexer not adding "??" or "??=" to line buffer

--- src/llex.cpp
+++ src/llex.cpp
@@ -764,9 +764,13 @@ static int llex (LexState *ls, SemInfo *seminfo) {
         next(ls);
         if (check_next1(ls, '?')) {
           if (check_next1(ls, '=')) {
+            ls->appendLineBuff("??=");
             ls->lasttoken = TK_COAL;
             return '=';
-          } else return TK_COAL; /* ?? */
+          } else {
+            ls->appendLineBuff("??");
+            return TK_COAL;
+          }
         }
         else {
           ls->appendLineBuff('?');
