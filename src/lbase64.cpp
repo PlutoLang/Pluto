1dcb40c5651fa361e6778dcbc7eeeaad9110af7f
Ryan Starrett <ryanmoople@gmail.com>
2022-10-16 19:18:47 -0400

    Add "base64" standard module, optional linkage with Soup.

---
+++ src/lbase64.cpp
@@ -0,0 +1,42 @@
+#include "lauxlib.h"
+#include "lualib.h"
+
+#ifdef PLUTO_USE_SOUP
+
+#include <string>
+#include <soup/base64.hpp>
+
+static int encode(lua_State* L) {
+	lua_pushstring(L, soup::base64::encode(luaL_checkstring(L, 1), (bool)lua_toboolean(L, 2)).c_str());
+	return 1;
+}
+
+static int decode(lua_State* L) {
+	lua_pushstring(L, soup::base64::decode(luaL_checkstring(L, 1)).c_str());
+	return 1;
+}
+
+static int urlEncode(lua_State* L) {
+	lua_pushstring(L, soup::base64::urlEncode(luaL_checkstring(L, 1), (bool)lua_toboolean(L, 2)).c_str());
+	return 1;
+}
+
+static int urlDecode(lua_State* L) {
+	lua_pushstring(L, soup::base64::urlDecode(luaL_checkstring(L, 1)).c_str());
+	return 1;
+}
+
+static const luaL_Reg funcs[] = {
+	{"url_encode", urlEncode},
+	{"url_decode", urlDecode},
+	{"encode", encode},
+	{"decode", decode},
+	{nullptr, nullptr}
+};
+
+LUAMOD_API int luaopen_base64(lua_State* L) {
+	luaL_newlib(L, funcs);
+	return 1;
+}
+
+#endif
