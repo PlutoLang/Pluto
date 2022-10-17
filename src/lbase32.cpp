87c9da2ac39f573395c34ab3702e22cc8fd2a755
Ryan Starrett <ryanmoople@gmail.com>
2022-10-17 05:25:43 -0400

    Add "base32" standard module.

---
+++ src/lbase32.cpp
@@ -0,0 +1,29 @@
+#include "lauxlib.h"
+#include "lualib.h"
+
+#ifdef PLUTO_USE_SOUP
+
+#include <soup/base32.hpp>
+
+static int encode(lua_State* L) {
+	lua_pushstring(L, soup::base32::encode(luaL_checkstring(L, 1), (bool)lua_toboolean(L, 2)).c_str());
+	return 1;
+}
+
+static int decode(lua_State* L) {
+	lua_pushstring(L, soup::base32::decode(luaL_checkstring(L, 1)).c_str());
+	return 1;
+}
+
+static const luaL_Reg funcs[] = {
+	{"encode", encode},
+	{"decode", decode},
+	{nullptr, nullptr}
+};
+
+LUAMOD_API int luaopen_base32(lua_State* L) {
+	luaL_newlib(L, funcs);
+	return 1;
+}
+
+#endif
