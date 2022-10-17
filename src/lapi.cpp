5c85f872538726c772e638fe62772028b2028fbc
Sainan <63328889+Sainan@users.noreply.github.com>
2022-10-18 00:12:56 +0200

    Fix code duplication

--- src/lapi.cpp
+++ src/lapi.cpp
@@ -558,20 +558,7 @@ LUA_API const char *lua_pushstring (lua_State *L, const char *s) {
 }
 
 LUA_API const char* lua_pushstring(lua_State* L, const std::string& str) {
-  const char* s = str.c_str();
-  lua_lock(L);
-  if (s == NULL)
-    setnilvalue(s2v(L->top));
-  else {
-    TString* ts;
-    ts = luaS_new(L, s);
-    setsvalue2s(L, L->top, ts);
-    s = getstr(ts);  /* internal copy's address */
-  }
-  api_incr_top(L);
-  luaC_checkGC(L);
-  lua_unlock(L);
-  return s;
+  return lua_pushstring(L, str.c_str());
 }
 
 
