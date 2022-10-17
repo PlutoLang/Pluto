9f7bc7b6ee6642c901560b386b9e7ca80ea199f9
Ryan Starrett <ryanmoople@gmail.com>
2022-10-16 20:57:21 -0400

    Add "base58" standard module.

---
+++ src/lbase58.cpp
@@ -0,0 +1,41 @@
+#include "lauxlib.h"
+#include "lualib.h"
+
+#ifdef PLUTO_USE_SOUP
+
+#include <soup/string.hpp>
+#include <soup/base58.hpp>
+
+static int decode(lua_State* L) {
+	try {
+		lua_pushstring(L, soup::string::bin2hex(soup::base58::decode(luaL_checkstring(L, 1))).c_str());
+	}
+	catch (...) {
+		luaL_error(L, "Attempted to decode non-base58 string.");
+	}
+	return 1;
+}
+
+static int is_valid(lua_State* L) {
+	try {
+		const auto discarding = soup::base58::decode(luaL_checkstring(L, 1));
+		lua_pushboolean(L, true);
+	}
+	catch (...) {
+		lua_pushboolean(L, false);
+	}
+	return 1;
+}
+
+static const luaL_Reg funcs[] = {
+	{"is_valid", is_valid},
+	{"decode", decode},
+	{nullptr, nullptr}
+};
+
+LUAMOD_API int luaopen_base58(lua_State* L) {
+	luaL_newlib(L, funcs);
+	return 1;
+}
+
+#endif
