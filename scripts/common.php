aadb3450cd3c62e276a30e9d3cab66f6c025f13d
Sainan <63328889+Sainan@users.noreply.github.com>
2023-03-14 19:54:31 +0100

    Fix PHP build scripts

--- scripts/common.php
+++ scripts/common.php
@@ -5,6 +5,14 @@ if(!is_dir("int"))
 {
 	mkdir("int");
 }
+if(!is_dir("int/vendor"))
+{
+	mkdir("int/vendor");
+}
+if(!is_dir("int/vendor/Soup"))
+{
+	mkdir("int/vendor/Soup");
+}
 
 function check_compiler()
 {
@@ -16,7 +24,7 @@ function check_compiler()
 	}
 
 	$compiler = resolve_installed_program($argv[1]);
-	$compiler .= " -std=c++17 -O3";
+	$compiler .= " -std=c++17 -D SOUP_CPP20=false -O3";
 	if(defined("PHP_WINDOWS_VERSION_MAJOR"))
 	{
 		$compiler .= " -D _CRT_SECURE_NO_WARNINGS";
@@ -38,13 +46,13 @@ function resolve_installed_program($exe)
 
 function for_each_obj($f)
 {
-	foreach(["src","src/vendor/Soup"] as $dir)
+	foreach(["","vendor/Soup/"] as $prefix)
 	{
-		foreach(scandir($dir) as $file)
+		foreach(scandir("src/".$prefix) as $file)
 		{
 			if(substr($file, -4) == ".cpp")
 			{
-				$f(substr($file, 0, -4));
+				$f($prefix.substr($file, 0, -4));
 			}
 		}
 	}
