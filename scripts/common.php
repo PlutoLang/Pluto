38890aeaabd65e52a0ee371d8804cdde7865a228
Sainan <63328889+Sainan@users.noreply.github.com>
2023-03-14 20:08:01 +0100

    Suppress unused command line argument warnings

--- scripts/common.php
+++ scripts/common.php
@@ -31,7 +31,7 @@ function check_compiler()
 	}
 	else
 	{
-		$compiler .= " -lm -lstdc++";
+		$compiler .= " -Wno-unused-command-line-argument -lm -lstdc++";
 		if (PHP_OS_FAMILY != "Darwin")
 		{
 			$compiler .= " -lstdc++fs -fuse-ld=lld";
