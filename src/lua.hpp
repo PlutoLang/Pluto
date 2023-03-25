a6c58f4128a446fcb32c18dac257247e806e83a2
Sainan <sainan@calamity.gg>
2023-03-25 17:46:52 +0100

    Remove extern "C" from lua.hpp since Pluto does not compile as C

--- src/lua.hpp
+++ src/lua.hpp
@@ -1,10 +1,5 @@
 #pragma once
-// lua.hpp
-// Lua header files for C++
-// <<extern "C">> not supplied automatically because Lua also compiles as C++
 
-extern "C" {
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
-}
