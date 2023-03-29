ba094b8b69bfd7dd541704c06741d85b36d67e06
Sainan <sainan@calamity.gg>
2023-03-29 23:48:12 +0200

    Handle errors gracefully, add tests

--- src/lualib.h
+++ src/lualib.h
@@ -72,3 +72,18 @@ LUAMOD_API int (luaopen_base64) (lua_State *L);
 
 /* open all previous libraries */
 LUALIB_API void (luaL_openlibs) (lua_State *L);
+
+/* utility for implementation of "universal" variants */
+#define pluto_uwrap(L, f, r) \
+  auto old = setlocale(LC_ALL, nullptr); \
+  setlocale(LC_NUMERIC, "en_US.UTF-8"); \
+  int narg = lua_gettop(L); \
+  lua_pushcfunction(L, f); \
+  lua_insert(L, 1); \
+  int status = lua_pcall(L, narg, r, 0); \
+  if (status != LUA_OK) { \
+    setlocale(LC_ALL, old); \
+    lua_error(L); \
+  } \
+  setlocale(LC_ALL, old); \
+  return r;
