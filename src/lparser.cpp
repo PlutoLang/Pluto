0e24a7e62672570f9b8bb903bbede6aa32fa77cc
Sainan <sainan@calamity.gg>
2023-04-05 20:03:53 +0200

    Ignore ';' for "unreachable code" warning

--- src/lparser.cpp
+++ src/lparser.cpp
@@ -3818,15 +3818,17 @@ static void retstat (LexState *ls, TypeDesc *prop) {
 static void statement (LexState *ls, TypeDesc *prop) {
   luaX_checkspecial(ls);
   int line = ls->getLineNumber();
-  if (ls->laststat.IsEscapingToken() ||
-     (ls->laststat.Is(TK_GOTO) && !ls->findWithinLine(line, luaX_lookbehind(ls).seminfo.ts->toCpp()))) /* Don't warn if this statement is the goto's label. */
+  if ((ls->laststat.IsEscapingToken()
+    || (ls->laststat.Is(TK_GOTO) && !ls->findWithinLine(line, luaX_lookbehind(ls).seminfo.ts->toCpp()))) /* Don't warn if this statement is the goto's label. */
+    && ls->t.token != ';') /* Don't warn if this is only a semicolon */
   {
     throw_warn(ls,
       "unreachable code",
         luaO_fmt(ls->L, "this code comes after an escaping %s statement.", luaX_token2str(ls, ls->laststat.token)), UNREACHABLE_CODE);
     ls->L->top--;
   }
-  ls->laststat.token = ls->t.token;
+  if (ls->t.token != ';')
+    ls->laststat.token = ls->t.token;
   enterlevel(ls);
   switch (ls->t.token) {
     case ';': {  /* stat -> ';' (empty statement) */
