dced6f901e057da71e1638e32080f8f8eb3c16e3
Sainan <sainan@calamity.gg>
2023-04-07 21:44:10 +0200

    Add array destructuring

--- src/lparser.cpp
+++ src/lparser.cpp
@@ -3634,7 +3634,6 @@ static void checktoclose (FuncState *fs, int level) {
 
 
 static void restdestructuring (LexState *ls, int line, std::vector<std::pair<TString*, expdesc>>& pairs) {
-  check_match(ls, '}', '{', line);
   checknext(ls, '=');
 
   /* begin scope of the locals we want to create */
@@ -3687,7 +3686,7 @@ static void destructuring (LexState *ls) {
   std::vector<std::pair<TString*, expdesc>> pairs{};
   luaX_next(ls); /* skip '{' */
   do {
-    TString* var = str_checkname(ls);
+    TString* var = str_checkname(ls, true);
     TString* prop = var;
     if (testnext(ls, '='))
       prop = str_checkname(ls);
@@ -3695,6 +3694,22 @@ static void destructuring (LexState *ls) {
     codestring(&propexp, prop);
     pairs.emplace_back(var, std::move(propexp));
   } while (testnext(ls, ','));
+  check_match(ls, '}', '{', line);
+  restdestructuring(ls, line, pairs);
+}
+
+static void arraydestructuring (LexState *ls) {
+  auto line = ls->getLineNumber();
+  std::vector<std::pair<TString*, expdesc>> pairs{};
+  luaX_next(ls); /* skip '[' */
+  expdesc prop;
+  init_exp(&prop, VKINT, 0);
+  prop.u.ival = 1;
+  do {
+    pairs.emplace_back(str_checkname(ls, true), prop);
+    prop.u.ival++;
+  } while (testnext(ls, ','));
+  check_match(ls, ']', '[', line);
   restdestructuring(ls, line, pairs);
 }
 
@@ -3702,6 +3717,10 @@ static void destructuring (LexState *ls) {
 static void localstat (LexState *ls) {
   if (ls->t.token == '{') {
     destructuring(ls);
+    return;
+  }
+  if (ls->t.token == '[') {
+    arraydestructuring(ls);
     return;
   }
   /* stat -> LOCAL NAME ATTRIB { ',' NAME ATTRIB } ['=' explist] */
