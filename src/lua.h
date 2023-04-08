88ee9fb7ecb1389e03abb71f9e5769fb49b201e8
Sainan <sainan@calamity.gg>
2023-04-08 02:12:28 +0200

    b5c65705ca78560cd2735778737122ea5f858bd0

--- src/lua.h
+++ src/lua.h
@@ -24,7 +24,7 @@
 
 #define LUA_VERSION	"Lua " LUA_VERSION_MAJOR "." LUA_VERSION_MINOR
 #define LUA_RELEASE	LUA_VERSION "." LUA_VERSION_RELEASE
-#define LUA_COPYRIGHT	PLUTO_VERSION " Copyright (C) 2022 Ryan Starrett, based on " LUA_RELEASE " Copyright (C) 1994-2022 Lua.org, PUC-Rio"
+#define LUA_COPYRIGHT	PLUTO_VERSION " Copyright (C) 2022-2023 Ryan Starrett, based on " LUA_RELEASE " Copyright (C) 1994-2023 Lua.org, PUC-Rio"
 #define LUA_AUTHORS	"R. Starrett, R. Ierusalimschy, L. H. de Figueiredo, W. Celes"
 
 
@@ -501,7 +501,7 @@ struct lua_Debug {
 
 
 /******************************************************************************
-* Copyright (C) 1994-2022 Lua.org, PUC-Rio.
+* Copyright (C) 1994-2023 Lua.org, PUC-Rio.
 *
 * Permission is hereby granted, free of charge, to any person obtaining
 * a copy of this software and associated documentation files (the
