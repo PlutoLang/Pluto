7b64b731a4fba0f9a94b0a65d9567517fbdeb0b4
Sainan <sainan@calamity.gg>
2023-04-23 12:25:05 +0200

    Fix not accepting 'pluto_use ... = true'

--- src/lparser.cpp
+++ src/lparser.cpp
@@ -4047,7 +4047,7 @@ static void usestat (LexState *ls) {
     if (testnext(ls, '=')) {
       if (testnext(ls, TK_FALSE))
         enable = false;
-      else checknext(ls, TK_FALSE);
+      else checknext(ls, TK_TRUE);
     }
     if (is_enabled != enable) {
       if (enable)
