f549dc5147f213c7210adf81ef2b37a0eded0d7b
Sainan <sainan@calamity.gg>
2023-08-25 10:13:47 +0200

    Add switch multi-case shorthand

--- src/lcode.h
+++ src/lcode.h
@@ -97,3 +97,4 @@ LUAI_FUNC void luaK_setlist (FuncState *fs, int base, int nelems, int tostore);
 LUAI_FUNC void luaK_finish (FuncState *fs);
 [[noreturn]] LUAI_FUNC void luaK_semerror (LexState *ls, const char *msg);
 LUAI_FUNC void luaK_exp2reg (FuncState *fs, expdesc *e, int reg);
+LUAI_FUNC void luaK_invertcond (FuncState *fs, int list);
