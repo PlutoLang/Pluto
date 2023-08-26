a756414a78c0e7599c3eb474dc465b69dac18fbc
Sainan <sainan@calamity.gg>
2023-08-26 05:54:12 +0200

    Raise warning if attributes are used without angle brackets

--- src/llex.h
+++ src/llex.h
@@ -186,6 +186,7 @@ enum WarningType : int
   WT_EXCESSIVE_ARGUMENTS,
   WT_DEPRECATED,
   WT_BAD_PRACTICE,
+  WT_POSSIBLE_TYPO,
 
   NUM_WARNING_TYPES
 };
@@ -199,6 +200,7 @@ inline const char* const luaX_warnNames[] = {
   "excessive-arguments",
   "deprecated",
   "bad-practice",
+  "possible-typo",
 };
 
 
