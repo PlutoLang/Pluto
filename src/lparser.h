ae2ab9accff773ebb9acce53039c226e09787b76
Sainan <sainan@calamity.gg>
2023-09-06 17:51:52 +0200

    Fix tripping assert in codeeq

--- src/lparser.h
+++ src/lparser.h
@@ -372,6 +372,7 @@ typedef struct FuncState {
   lu_byte freereg;  /* first free register */
   lu_byte iwthabs;  /* instructions issued since last absolute line info */
   lu_byte needclose;  /* function needs to close upvalues when returning */
+  int pinnedreg;
 } FuncState;
 
 
