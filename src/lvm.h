45597431771bc010311d5692fd210d7b4a51129e
well-in-that-case <ryanmoople@gmail.com>
2023-09-30 15:23:42 -0400

    Fix OP_IN's misguided table search logic

--- src/lvm.h
+++ src/lvm.h
@@ -134,3 +134,4 @@ LUAI_FUNC lua_Integer luaV_mod (lua_State *L, lua_Integer x, lua_Integer y);
 LUAI_FUNC lua_Number luaV_modf (lua_State *L, lua_Number x, lua_Number y);
 LUAI_FUNC lua_Integer luaV_shiftl (lua_Integer x, lua_Integer y);
 LUAI_FUNC void luaV_objlen (lua_State *L, StkId ra, const TValue *rb);
+LUAI_FUNC bool luaV_searchelement(lua_State* L, const Table* t, const TValue* element);
