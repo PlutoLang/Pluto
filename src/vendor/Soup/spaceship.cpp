1dd8e1b1cc11ae1d6d6c00617b4d9ee8d059bb56
Sainan <sainan@calamity.gg>
2023-11-13 23:30:14 +0100

    Add version_compare

---
+++ src/vendor/Soup/spaceship.cpp
@@ -0,0 +1,10 @@
+#include "spaceship.hpp"
+
+#if !SOUP_SPACESHIP_USE_STD
+namespace soup
+{
+	const strong_ordering strong_ordering::less = -1;
+	const strong_ordering strong_ordering::equal = 0;
+	const strong_ordering strong_ordering::greater = 1;
+}
+#endif
---
+++ src/vendor/Soup/version_compare.cpp
@@ -0,0 +1,47 @@
+#include "version_compare.hpp"
+
+#include "string.hpp"
+
+namespace soup
+{
+	strong_ordering version_compare(std::string in_a, std::string in_b)
+	{
+		std::vector<long> a{};
+		std::vector<long> b{};
+
+		string::replaceAll(in_a, "-", ".");
+		string::replaceAll(in_b, "-", ".");
+
+		for (const auto& s : string::explode(in_a, '.'))
+		{
+			a.emplace_back(string::toInt<long>(s, -1));
+		}
+		for (const auto& s : string::explode(in_b, '.'))
+		{
+			b.emplace_back(string::toInt<long>(s, -1));
+		}
+
+		if (a.size() != b.size())
+		{
+			if (a.size() > b.size())
+			{
+				b.insert(b.end(), a.size() - b.size(), 0);
+			}
+			else
+			{
+				a.insert(a.end(), b.size() - a.size(), 0);
+			}
+		}
+
+		for (size_t i = 0; i != a.size(); ++i)
+		{
+			auto c = SOUP_SPACESHIP(a[i], b[i]);
+			if (c != 0)
+			{
+				return c;
+			}
+		}
+
+		return strong_ordering::equal;
+	}
+}
