f4f401ecb2a95a255c89e4bddbff20ebeda1bbbc
Sainan <sainan@calamity.gg>
2023-11-14 21:29:25 +0100

    Fix C++ 20 build

--- src/vendor/Soup/spaceship.hpp
+++ src/vendor/Soup/spaceship.hpp
@@ -8,22 +8,25 @@
 #include <compare>
 #endif
 
+#if SOUP_SPACESHIP_USE_STD
 namespace soup
 {
-#if SOUP_SPACESHIP_USE_STD
 	using strong_ordering = ::std::strong_ordering;
+}
 
-	#define SOUP_SPACESHIP(a, b) ((a) <=> (b))
+#define SOUP_SPACESHIP(a, b) ((a) <=> (b))
 
-	[[nodiscard]] inline int SOUP_STRONG_ORDERING_TO_INT(strong_ordering so)
+[[nodiscard]] inline int SOUP_STRONG_ORDERING_TO_INT(soup::strong_ordering so)
+{
+	if (so != 0)
 	{
-		if (so != 0)
-		{
-			return so < 0 ? -1 : 1;
-		}
-		return 0;
+		return so < 0 ? -1 : 1;
 	}
+	return 0;
+}
 #else
+namespace soup
+{
 	class strong_ordering
 	{
 	private:
@@ -44,21 +47,21 @@ namespace soup
 			return value;
 		}
 	};
-
-	template <typename T>
-	[[nodiscard]] inline strong_ordering SOUP_SPACESHIP(const T& a, const T& b)
-	{
-		if (a == b)
-		{
-			return strong_ordering::equal;
-		}
-		if (a < b)
-		{
-			return strong_ordering::less;
-		}
-		return strong_ordering::greater;
-	}
-
-	#define SOUP_STRONG_ORDERING_TO_INT(so) ((so).operator int())
-#endif
 }
+
+template <typename T>
+[[nodiscard]] inline soup::strong_ordering SOUP_SPACESHIP(const T& a, const T& b)
+{
+	if (a == b)
+	{
+		return soup::strong_ordering::equal;
+	}
+	if (a < b)
+	{
+		return soup::strong_ordering::less;
+	}
+	return soup::strong_ordering::greater;
+}
+
+#define SOUP_STRONG_ORDERING_TO_INT(so) ((so).operator int())
+#endif
