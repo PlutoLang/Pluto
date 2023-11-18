dbff006047ac8e074804af75200e58b0f33763b5
Sainan <sainan@calamity.gg>
2023-11-18 03:49:52 +0100

    Reuse expdesc

--- src/lparser.cpp
+++ src/lparser.cpp
@@ -2291,7 +2291,6 @@ static void safe_navigation(LexState *ls, expdesc *v) {
         break;
       }
       case ':': {
-        expdesc key;
         luaX_next(ls);
         codename(ls, &key);
         luaK_self(fs, v, &key);
