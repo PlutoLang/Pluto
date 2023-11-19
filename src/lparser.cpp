057a5dc1f436fbd9e72c3e0c58c4b4b5d7d6e57a
Sainan <sainan@calamity.gg>
2023-11-19 21:57:20 +0100

    Fix treating new statement like it's a return

--- src/lparser.cpp
+++ src/lparser.cpp
@@ -4841,7 +4841,6 @@ static void statement (LexState *ls, TypeHint *prop) {
     }
     case TK_NEW:
     case TK_PNEW: {
-      if (prop) prop->emplaceTypeDesc(VT_TABLE);
       expdesc v;
       newexpr(ls, &v);
       expsuffix(ls, &v, line, 0, prop);
