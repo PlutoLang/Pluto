529746feb8d0074d9a49a94b9a6b487bef605880
Sainan <sainan@calamity.gg>
2023-12-10 00:50:39 +0100

    Fix comment

--- src/lcode.cpp
+++ src/lcode.cpp
@@ -2044,6 +2044,6 @@ void luaK_invertcond (FuncState *fs, int list) {
 
 
 void luaK_settop (FuncState *fs, int reg) {
-  /* OP_CONCAT with R(B) = 1 is a no-op BUT it does set L->top to R(A) - R(B), so that's a bytecode-compatible 'set top' for us. :) */
+  /* OP_CONCAT with R(B) = 1 is a no-op BUT it does set L->top to R(A) + R(B), so that's a bytecode-compatible 'set top' for us. :) */
   luaK_codeABC(fs, OP_CONCAT, reg - 1, 1, 0);
 }
