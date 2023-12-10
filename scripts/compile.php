4762bacbe0e3e4aa9313137c355fb353d04da314
Sainan <sainan@calamity.gg>
2023-12-10 10:35:36 +0100

    Multi-thread compile script

--- scripts/compile.php
+++ scripts/compile.php
@@ -1,9 +1,29 @@
 <?php
 require __DIR__."/common.php";
 check_compiler();
+$procs = [];
+$descriptorspec = array(
+	0 => array("pipe", "r"),
+	1 => array("pipe", "w"),
+	2 => array("pipe", "w"),
+);
 for_each_obj(function($file)
 {
-	echo "$file\n";
-	global $compiler;
-	passthru($compiler." -o int/{$file}.o -c src/{$file}.cpp");
+	//echo "$file\n";
+	global $compiler, $procs, $descriptorspec;
+	$procs[$file] = proc_open($compiler." -o int/{$file}.o -c src/{$file}.cpp", $descriptorspec, $pipes);
 });
+$any_running = false;
+do
+{
+	usleep(50000);
+	$any_running = false;
+	foreach ($procs as $proc)
+	{
+		if (proc_get_status($proc)["running"])
+		{
+			$any_running = true;
+			break;
+		}
+	}
+} while ($any_running);
