61841e33c88820d5a84ff89bcab93cff139af394
Sainan <sainan@calamity.gg>
2023-12-10 10:42:53 +0100

    Abstractify

--- scripts/compile.php
+++ scripts/compile.php
@@ -1,29 +1,9 @@
 <?php
 require __DIR__."/common.php";
 check_compiler();
-$procs = [];
-$descriptorspec = array(
-	0 => array("pipe", "r"),
-	1 => array("pipe", "w"),
-	2 => array("pipe", "w"),
-);
 for_each_obj(function($file)
 {
-	//echo "$file\n";
-	global $compiler, $procs, $descriptorspec;
-	$procs[$file] = proc_open($compiler." -o int/{$file}.o -c src/{$file}.cpp", $descriptorspec, $pipes);
+	global $compiler;
+	run_command_async($compiler." -o int/{$file}.o -c src/{$file}.cpp");
 });
-$any_running = false;
-do
-{
-	usleep(50000);
-	$any_running = false;
-	foreach ($procs as $proc)
-	{
-		if (proc_get_status($proc)["running"])
-		{
-			$any_running = true;
-			break;
-		}
-	}
-} while ($any_running);
+await_commands();
