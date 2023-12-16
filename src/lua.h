56224b82dda627458c6db9f2b1a96775dd471bb8
Sainan <sainan@calamity.gg>
2023-12-16 02:44:30 +0100

    Bump version

--- src/lua.h
+++ src/lua.h
@@ -12,7 +12,7 @@
 #include <string_view>
 
 
-#define PLUTO_VERSION "Pluto 0.8.0"
+#define PLUTO_VERSION "Pluto 0.9.0"
 
 #define LUA_COPYRIGHT	PLUTO_VERSION " Copyright (C) 2022-2023 PlutoLang.org, Ryan Starrett, Sainan.\r\nBased on " LUA_RELEASE " Copyright (C) 1994-2023 Lua.org, PUC-Rio."
 #define LUA_AUTHORS	"R. Starrett, R. Ierusalimschy, L. H. de Figueiredo, W. Celes"
