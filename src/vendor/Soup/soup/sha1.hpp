60062a9471f11e7803ddac6d655047c358b4953d
Sainan <sainan@calamity.gg>
2023-12-16 03:39:11 +0100

    Add crypto.sha1

---
+++ src/vendor/Soup/soup/sha1.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "fwd.hpp"
+#include "CryptoHashAlgo.hpp"
+
+namespace soup
+{
+	struct sha1 : public CryptoHashAlgo<sha1>
+	{
+		static constexpr unsigned char OID[] = { 0x30, 0x21, 0x30, 0x09, 0x06, 0x05, 0x2B, 0x0E, 0x03, 0x02, 0x1A, 0x05, 0x00, 0x04, 0x14 };
+		static constexpr auto DIGEST_BYTES = 20u;
+		static constexpr auto BLOCK_BYTES = 64u;
+
+		[[nodiscard]] static std::string hash(const std::string& str);
+		[[nodiscard]] static std::string hash(ioSeekableReader& r);
+	};
+}
