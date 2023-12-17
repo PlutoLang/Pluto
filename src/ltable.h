8c1a52daacf6e144f40af24ef4d8932d665a4dbe
Sainan <sainan@calamity.gg>
2023-12-17 06:12:50 +0100

    Add table.size

--- src/ltable.h
+++ src/ltable.h
@@ -52,6 +52,7 @@ LUAI_FUNC void luaH_free (lua_State *L, Table *t);
 LUAI_FUNC int luaH_next (lua_State *L, Table *t, StkId key);
 LUAI_FUNC lua_Unsigned luaH_getn (Table *t);
 LUAI_FUNC unsigned int luaH_realasize (const Table *t);
+LUAI_FUNC unsigned int luaH_gethsize (const Table *t);
 
 
 #if defined(LUA_DEBUG)
