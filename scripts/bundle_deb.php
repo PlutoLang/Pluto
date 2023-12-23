f491d5f40f4cb69eb7bd439c123776af3915a712
Sainan <sainan@calamity.gg>
2023-12-23 06:01:53 +0100

    Also bundle static lib & headers in deb package

--- scripts/bundle_deb.php
+++ scripts/bundle_deb.php
@@ -13,6 +13,12 @@ $arch = trim(shell_exec("dpkg --print-architecture"));
 
 mkdir("pluto");
 mkdir("pluto/DEBIAN");
+mkdir("pluto/usr");
+mkdir("pluto/usr/bin");
+mkdir("pluto/usr/lib");
+mkdir("pluto/usr/include");
+mkdir("pluto/usr/include/pluto");
+
 file_put_contents("pluto/DEBIAN/control", <<<EOC
 Package: pluto
 Version: $pluto_version
@@ -25,10 +31,18 @@ Description: A superset of Lua 5.4 — with unique features, optimizations, and 
 
 EOC);
 chmod("pluto/DEBIAN/control", 0644);
-mkdir("pluto/usr");
-mkdir("pluto/usr/bin");
+
 copy("src/pluto", "pluto/usr/bin/pluto");
 copy("src/plutoc", "pluto/usr/bin/plutoc");
 chmod("pluto/usr/bin/pluto", 0755);
 chmod("pluto/usr/bin/plutoc", 0755);
+
+copy("src/libpluto.a", "pluto/usr/lib/libpluto.a");
+
+copy("src/lua.h", "pluto/usr/include/pluto/lua.h");
+copy("src/lua.hpp", "pluto/usr/include/pluto/lua.hpp");
+copy("src/lualib.h", "pluto/usr/include/pluto/lualib.h");
+copy("src/lauxlib.h", "pluto/usr/include/pluto/lauxlib.h");
+copy("src/luaconf.h", "pluto/usr/include/pluto/luaconf.h");
+
 passthru("dpkg-deb --build pluto");
