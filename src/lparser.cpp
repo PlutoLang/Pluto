dafdfe67c908b7116a6fc1c61e606768972a8eb6
Sainan <sainan@calamity.gg>
2023-12-31 12:20:10 +0100

    Allow pipe operator after table constructor

--- src/lparser.cpp
+++ src/lparser.cpp
@@ -3202,7 +3202,7 @@ static void simpleexp (LexState *ls, expdesc *v, int flags, TypeHint *prop) {
     case '{': {  /* constructor */
       if (prop) prop->emplaceTypeDesc(VT_TABLE);
       constructor(ls, v);
-      if (ls->t.token == '[' || ls->t.token == ':' || ls->t.token == '.')
+      if (ls->t.token == '[' || ls->t.token == ':' || ls->t.token == '.' || ls->t.token == TK_PIPE)
         break;
       return;
     }
