9da42f98ef348575e5213ecdd32ee1eea2d4ff8f
Sainan <sainan@calamity.gg>
2024-02-07 00:46:10 +0100

    024f9064f1b43758eb36aba52547edc0312bf4ba

--- src/lstring.h
+++ src/lstring.h
@@ -24,12 +24,6 @@
 #define sizestrshr(l)  \
 	(offsetof(TString, contents) + ((l) + 1) * sizeof(char))
 
-/*
-** Size of a long TString: Size of the header plus space for the string
-** itself (including final '\0').
-*/
-#define sizestrlng(l)	(sizeof(TString) + ((l) + 1) * sizeof(char))
-
 
 #define luaS_newliteral(L, s)	(luaS_newlstr(L, "" s, \
                                  (sizeof(s)/sizeof(char))-1))
@@ -62,6 +56,9 @@ LUAI_FUNC Udata *luaS_newudata (lua_State *L, size_t s, int nuvalue);
 LUAI_FUNC TString *luaS_newlstr (lua_State *L, const char *str, size_t l);
 LUAI_FUNC TString *luaS_new (lua_State *L, const char *str);
 LUAI_FUNC TString *luaS_createlngstrobj (lua_State *L, size_t l);
+LUAI_FUNC TString *luaS_newextlstr (lua_State *L,
+		const char *s, size_t len, lua_Alloc falloc, void *ud);
+LUAI_FUNC size_t luaS_sizelngstr (size_t len, int kind);
 
 LUAI_FUNC char *plutoS_prealloc (lua_State *L, char shrtbuf[LUAI_MAXSHORTLEN], size_t l);
 LUAI_FUNC void plutoS_commit (lua_State *L, char *prealloc, size_t l);
