1204ae8a5329d972fc3b94b035aa337f34b5f02d
Sainan <sainan@calamity.gg>
2024-02-11 23:03:47 +0100

    Add "rsa" and "rsa-pkcs1" modes to crypto.encrypt & crypto.decrypt

--- src/lcryptolib.cpp
+++ src/lcryptolib.cpp
@@ -442,6 +442,36 @@ static int l_encrypt (lua_State *L) {
     }
     return 1;
   }
+  else if (mode_len >= 3 && memcmp(mode, "rsa", 3) == 0) {
+    luaL_checktype(L, 2, LUA_TTABLE);
+    soup::Bigint *n = lua_getfield(L, 2, "n") == LUA_TUSERDATA ? checkbigint(L, -1) : nullptr; if (n) lua_pop(L, 1);
+    soup::Bigint *e = lua_getfield(L, 2, "e") == LUA_TUSERDATA ? checkbigint(L, -1) : nullptr; if (e) lua_pop(L, 1);
+    soup::Bigint *p = lua_getfield(L, 2, "p") == LUA_TUSERDATA ? checkbigint(L, -1) : nullptr; if (p) lua_pop(L, 1);
+    soup::Bigint *q = lua_getfield(L, 2, "q") == LUA_TUSERDATA ? checkbigint(L, -1) : nullptr; if (q) lua_pop(L, 1);
+    if (p && q) {  /* private key? */
+      std::string data = pluto_checkstring(L, 3);
+      if (strcmp(mode, "rsa-pkcs1") == 0) {
+        data = soup::RsaPrivateKey::fromPrimes(*p, *q).encryptPkcs1(std::move(data)).toBinary();
+      }
+      else {
+        data = soup::RsaPrivateKey::fromPrimes(*p, *q).encryptUnpadded(std::move(data)).toBinary();
+      }
+      pluto_pushstring(L, data);
+	  return 1;
+    }
+    else if (n && e) {  /* public key? */
+      std::string data = pluto_checkstring(L, 3);
+      if (strcmp(mode, "rsa-pkcs1") == 0) {
+        data = soup::RsaPublicKey(*n, *e).encryptPkcs1(std::move(data)).toBinary();
+      }
+      else {
+        data = soup::RsaPublicKey(*n, *e).encryptUnpadded(std::move(data)).toBinary();
+      }
+      pluto_pushstring(L, data);
+      return 1;
+    }
+    else luaL_error(L, "Invalid key");
+  }
   else luaL_error(L, "Unknown mode");
 }
 
@@ -548,6 +578,36 @@ static int l_decrypt (lua_State *L) {
 
     lua_pushlstring(L, data, data_len);
     return 1;
+  }
+  else if (mode_len >= 3 && memcmp(mode, "rsa", 3) == 0) {
+    luaL_checktype(L, 2, LUA_TTABLE);
+    soup::Bigint *n = lua_getfield(L, 2, "n") == LUA_TUSERDATA ? checkbigint(L, -1) : nullptr; if (n) lua_pop(L, 1);
+    soup::Bigint *e = lua_getfield(L, 2, "e") == LUA_TUSERDATA ? checkbigint(L, -1) : nullptr; if (e) lua_pop(L, 1);
+    soup::Bigint *p = lua_getfield(L, 2, "p") == LUA_TUSERDATA ? checkbigint(L, -1) : nullptr; if (p) lua_pop(L, 1);
+    soup::Bigint *q = lua_getfield(L, 2, "q") == LUA_TUSERDATA ? checkbigint(L, -1) : nullptr; if (q) lua_pop(L, 1);
+    if (p && q) {  /* private key? */
+      std::string data = pluto_checkstring(L, 3);
+      if (strcmp(mode, "rsa-pkcs1") == 0) {
+        data = soup::RsaPrivateKey::fromPrimes(*p, *q).decryptPkcs1(soup::Bigint::fromBinary(data));
+      }
+      else {
+        data = soup::RsaPrivateKey::fromPrimes(*p, *q).decryptUnpadded(soup::Bigint::fromBinary(data));
+      }
+      pluto_pushstring(L, data);
+	  return 1;
+    }
+    else if (n && e) {  /* public key? */
+      std::string data = pluto_checkstring(L, 3);
+      if (strcmp(mode, "rsa-pkcs1") == 0) {
+        data = soup::RsaPublicKey(*n, *e).decryptPkcs1(soup::Bigint::fromBinary(data));
+      }
+      else {
+        data = soup::RsaPublicKey(*n, *e).decryptUnpadded(soup::Bigint::fromBinary(data));
+      }
+      pluto_pushstring(L, data);
+      return 1;
+    }
+    else luaL_error(L, "Invalid key");
   }
   else luaL_error(L, "Unknown mode");
 }
