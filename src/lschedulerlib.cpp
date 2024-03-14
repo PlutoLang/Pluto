1e8ec7aad6aae1c287114804f0aec96fc6f3530b
Sainan <sainan@calamity.gg>
2024-03-14 21:19:01 +0100

    Make scheduler a class instead of a singleton

--- src/lschedulerlib.cpp
+++ src/lschedulerlib.cpp
@@ -10,45 +10,52 @@ static const luaL_Reg funcs[] = {
 LUAMOD_API int luaopen_scheduler (lua_State *L) {
 	const auto code = R"EOC(pluto_use "0.6.0"
 
-local scheduler = {}
-
-local coros = {}
 local function resume(coro)
     if select("#", coroutine.xresume(coro)) ~= 0 then
-        warn("Coroutine yielded values to scheduler lib. Discarding them.")
+        warn("Coroutine yielded values to scheduler. Discarding them.")
     end
 end
-local function add(f)
-    local coro = coroutine.create(f)
-    table.insert(coros, coro)
-    resume(coro)
-    return coro
-end
-scheduler.add = add
-function scheduler.addloop(f)
-    return add(function()
-        while f() ~= false do
-            coroutine.yield()
-        end
-    end)
-end
-scheduler.yieldfunc = || -> do os.sleep(1) end
-function scheduler.run()
-    local all_dead
-    repeat
-        all_dead = true
-        for i, coro in coros do
-            if coroutine.status(coro) == "suspended" then
-                resume(coro)
-                all_dead = false
-            elseif coroutine.status(coro) == "dead" then
-                coros[i] = nil
+
+return class
+    __name = "pluto:scheduler"
+
+    yieldfunc = || -> do os.sleep(1) end
+
+    function __construct()
+        self.coros = {}
+    end
+
+    function add(f)
+        local coro = coroutine.create(f)
+        table.insert(self.coros, coro)
+        resume(coro)
+        return coro
+    end
+
+    function addloop(f)
+        return self:add(function()
+            while f() ~= false do
+                coroutine.yield()
             end
-        end
-        scheduler.yieldfunc()
-    until all_dead
-end
-return scheduler)EOC";
+        end)
+    end
+
+    function run()
+        local all_dead
+        repeat
+            all_dead = true
+            for i, coro in self.coros do
+                if coroutine.status(coro) == "suspended" then
+                    resume(coro)
+                    all_dead = false
+                elseif coroutine.status(coro) == "dead" then
+                    coros[i] = nil
+                end
+            end
+            self.yieldfunc()
+        until all_dead
+    end
+end)EOC";
 	luaL_loadbuffer(L, code, strlen(code), "pluto:scheduler");
 	lua_call(L, 0, 1);
 	return 1;
