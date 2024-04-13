0d2abc95e6b0d086ae85c4bde57c11eb06377d9d
Sainan <sainan@calamity.gg>
2024-04-13 02:52:13 +0200

    Mandate usage of $else and $end for $if statement

--- src/lparser.cpp
+++ src/lparser.cpp
@@ -4295,9 +4295,7 @@ static void constexprifstat (LexState *ls, int line) {
   else {
     skip_block(ls);
   }
-  if (!testnext(ls, '$')) {
-    throw_warn(ls, "Compile-time control flow statement should be prefixed with a $", WT_DEPRECATED);
-  }
+  checknext(ls, '$');
   if (testnext(ls, TK_ELSE)) {
     if (disposition == false) {
       block(ls);
@@ -4305,9 +4303,7 @@ static void constexprifstat (LexState *ls, int line) {
     else {
       skip_block(ls);
     }
-    if (!testnext(ls, '$')) {
-      throw_warn(ls, "Compile-time control flow statement should be prefixed with a $", WT_DEPRECATED);
-    }
+    checknext(ls, '$');
   }
   check_match(ls, TK_END, TK_IF, line);
 }
