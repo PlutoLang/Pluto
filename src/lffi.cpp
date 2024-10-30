PlutoLang/Pluto

commit dc8bbd14fbf645eec008f01e14df6e2835141709

FFI: Fix reporting "malformed function" when symbol was not found

src/lffi.cpp
@@ -287,28 +287,28 @@ static int ffi_lib_cdef (lua_State *L) {
     if (par->align(), par->i->isLiteral() && par->i->val.getString() == "(") {
       par->i = i;
       try {
-        *func = par->readFunc();
-        pluto_pushstring(L, func->name);
-        auto fw = newfuncwrapper(L);
-        fw->ret = rfl_type_to_ffi_type(func->return_type);
-        SOUP_ASSERT(fw->ret != FFI_UNKNOWN);
-        fw->addr = lib->getAddress(func->name.c_str());
-        if (l_unlikely(fw->addr == nullptr)) {
-          luaL_error(L, "could not find '%s' in library", func->name.c_str());
-        }
-        if (func->parameters.size() > soup::ffi::MAX_ARGS) {
-          luaL_error(L, "'%s' has too many parameters", func->name.c_str());
-        }
-        fw->args.reserve(func->parameters.size());
-        for (const auto& param : func->parameters) {
-          fw->args.emplace_back(rfl_type_to_ffi_type(param.type));
-          SOUP_ASSERT(fw->args.back() != FFI_UNKNOWN);
-        }
-        lua_settable(L, 1);
+          *func = par->readFunc();
       }
       catch (...) {
         luaL_error(L, "malformed function");
       }
+      pluto_pushstring(L, func->name);
+      auto fw = newfuncwrapper(L);
+      fw->ret = rfl_type_to_ffi_type(func->return_type);
+      SOUP_ASSERT(fw->ret != FFI_UNKNOWN);
+      fw->addr = lib->getAddress(func->name.c_str());
+      if (l_unlikely(fw->addr == nullptr)) {
+        luaL_error(L, "could not find '%s' in library", func->name.c_str());
+      }
+      if (func->parameters.size() > soup::ffi::MAX_ARGS) {
+        luaL_error(L, "'%s' has too many parameters", func->name.c_str());
+      }
+      fw->args.reserve(func->parameters.size());
+      for (const auto& param : func->parameters) {
+        fw->args.emplace_back(rfl_type_to_ffi_type(param.type));
+        SOUP_ASSERT(fw->args.back() != FFI_UNKNOWN);
+      }
+      lua_settable(L, 1);
     }
     else {
       void *addr = lib->getAddress(var->name.c_str());

testes/pluto/ffi/test.pluto
@@ -100,3 +100,10 @@ struct PointsAt {
 local pointed_at = ffi.new("PointedAt")
 local points_at = ffi.new("PointsAt")
 points_at.target = pointed_at
+
+try
+    lib:cdef[[void non_existent();]]
+    assert(false) -- unreachable, cdef should throw
+catch e then
+    assert("could not find" in e)
+end

commit 9d644901265830271ad8301e45d77c0f18abbcda

FFI: Fix (error message not a string) when dealing with FFI_UNKNOWN Introduced in dc8bbd14fbf645eec008f01e14df6e2835141709

src/lffi.cpp
@@ -295,7 +295,8 @@ static int ffi_lib_cdef (lua_State *L) {
       pluto_pushstring(L, func->name);
       auto fw = newfuncwrapper(L);
       fw->ret = rfl_type_to_ffi_type(func->return_type);
-      SOUP_ASSERT(fw->ret != FFI_UNKNOWN);
+      if (l_unlikely(fw->ret == FFI_UNKNOWN))
+        luaL_error(L, "malformed function");
       fw->addr = lib->getAddress(func->name.c_str());
       if (l_unlikely(fw->addr == nullptr)) {
         luaL_error(L, "could not find '%s' in library", func->name.c_str());
@@ -306,7 +307,8 @@ static int ffi_lib_cdef (lua_State *L) {
       fw->args.reserve(func->parameters.size());
       for (const auto& param : func->parameters) {
         fw->args.emplace_back(rfl_type_to_ffi_type(param.type));
-        SOUP_ASSERT(fw->args.back() != FFI_UNKNOWN);
+        if (l_unlikely(fw->args.back() == FFI_UNKNOWN))
+          luaL_error(L, "malformed function");
       }
       lua_settable(L, 1);
     }