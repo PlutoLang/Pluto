29a80717cb69ecc593f3d9a611c0fd62b837ed7b
Sainan <sainan@calamity.inc>
2024-11-13 08:33:37 +0100

    Respect PLUTO_MEMORY_LIMIT for buffer library

--- src/vendor/Soup/soup/alloc.cpp
+++ src/vendor/Soup/soup/alloc.cpp
@@ -1,8 +1,27 @@
 #include "alloc.hpp"
 
-#include "memCAllocator.hpp"
+#include <cstdlib> // malloc, realloc
+#include <new> // bad_alloc
 
 NAMESPACE_SOUP
 {
-	memAllocator g_allocator = memCAllocator();
+	void* malloc(size_t size) /* SOUP_EXCAL */
+	{
+		void* ptr = ::malloc(size);
+		SOUP_IF_LIKELY(ptr)
+		{
+			return ptr;
+		}
+		SOUP_THROW(std::bad_alloc{});
+	}
+
+	void* realloc(void* ptr, size_t new_size) /* SOUP_EXCAL */
+	{
+		ptr = ::realloc(ptr, new_size);
+		SOUP_IF_LIKELY(ptr)
+		{
+			return ptr;
+		}
+		SOUP_THROW(std::bad_alloc{});
+	}
 }
