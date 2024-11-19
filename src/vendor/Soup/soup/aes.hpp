34335a982a24df5e034a10a3e67743ca0c3950bf
Sainan <sainan@calamity.inc>
2024-11-19 10:19:51 +0100

    Update Soup

--- src/vendor/Soup/soup/aes.hpp
+++ src/vendor/Soup/soup/aes.hpp
@@ -2,15 +2,13 @@
 
 #include <cstdint>
 #include <string>
-#include <vector>
 
 #include "base.hpp" // SOUP_EXCAL
 
 NAMESPACE_SOUP
 {
-	class aes
+	struct aes
 	{
-	public:
 		// Input size should be a multiple of 16 bytes.
 		//   GCM can deal with unaligned data, other methods will simply ignore the trailing bytes -> they will not be encrypted.
 		//   You may use a padding scheme such as PKCS#7 for padding.
@@ -25,10 +23,10 @@ NAMESPACE_SOUP
 		static void cfbDecrypt(uint8_t* data, size_t data_len, const uint8_t* key, size_t key_len, const uint8_t iv[16]) noexcept;
 		static void ecbEncrypt(uint8_t* data, size_t data_len, const uint8_t* key, size_t key_len) noexcept;
 		static void ecbDecrypt(uint8_t* data, size_t data_len, const uint8_t* key, size_t key_len) noexcept;
-		static void gcmEncrypt(uint8_t* data, size_t data_len, const uint8_t* aadata, size_t aadata_len, const uint8_t* key, size_t key_len, const uint8_t* iv, size_t iv_len, uint8_t tag[16]) SOUP_EXCAL;
-		static bool gcmDecrypt(uint8_t* data, size_t data_len, const uint8_t* aadata, size_t aadata_len, const uint8_t* key, size_t key_len, const uint8_t* iv, size_t iv_len, const uint8_t tag[16]) SOUP_EXCAL;
+		static void gcmEncrypt(uint8_t* data, size_t data_len, const uint8_t* aadata, size_t aadata_len, const uint8_t* key, size_t key_len, const uint8_t* iv, size_t iv_len, uint8_t tag[16]) noexcept;
+		static bool gcmDecrypt(uint8_t* data, size_t data_len, const uint8_t* aadata, size_t aadata_len, const uint8_t* key, size_t key_len, const uint8_t* iv, size_t iv_len, const uint8_t tag[16]) noexcept;
 
-	private:
+
 		static void expandKey(uint8_t w[240], const uint8_t* key, size_t key_len) noexcept;
 		static void expandKeyForDecryption(uint8_t w[240], const uint8_t* key, size_t key_len) noexcept;
 		[[nodiscard]] static int getNk(size_t key_len) noexcept;
@@ -53,11 +51,42 @@ NAMESPACE_SOUP
 		static SOUP_FORCEINLINE void xorBlocks(uint8_t a[16], const uint8_t b[16]) noexcept;
 		static void xorBlocks(uint8_t a[], const uint8_t b[], unsigned int len) noexcept;
 
-		static void ghash(uint8_t res[16], const uint8_t h[16], const std::vector<uint8_t>& x) noexcept;
+		static SOUP_FORCEINLINE void rshiftBlock(uint8_t block[16]) noexcept;
+		static void mulBlocks(uint8_t res[16], const uint8_t x[16], const uint8_t y[16]) noexcept;
 		static void calcH(uint8_t h[16], uint8_t roundKeys[240], const int Nr) noexcept;
-		static void calcJ0(uint8_t j0[16], const uint8_t h[16], const uint8_t* iv, size_t iv_len) SOUP_EXCAL;
+		static void calcJ0(uint8_t j0[16], const uint8_t h[16], const uint8_t* iv, size_t iv_len) noexcept;
 		static void inc32(uint8_t block[16]) noexcept;
 		static void gctr(uint8_t* data, size_t data_len, const uint8_t roundKeys[240], const int Nr, const uint8_t icb[8]) noexcept;
-		static void calcGcmTag(uint8_t tag[16], uint8_t* data, size_t data_len, const uint8_t* aadata, size_t aadata_len, const uint8_t roundKeys[16], const int Nr, const uint8_t h[16], const uint8_t j0[16]) SOUP_EXCAL;
+		static void calcGcmTag(uint8_t tag[16], uint8_t* data, size_t data_len, const uint8_t* aadata, size_t aadata_len, const uint8_t roundKeys[16], const int Nr, const uint8_t h[16], const uint8_t j0[16]) noexcept;
+
+		struct GhashState
+		{
+			uint8_t* const res;
+			const uint8_t* const h;
+			uint8_t buffer[16];
+			uint8_t buffer_counter;
+
+			GhashState(uint8_t res[16], const uint8_t h[16]) noexcept;
+
+			void append(const uint8_t data[], size_t size) noexcept
+			{
+				for (size_t i = 0; i != size; ++i)
+				{
+					appendByte(data[i]);
+				}
+			}
+			
+			void appendByte(uint8_t byte) noexcept
+			{
+				buffer[buffer_counter] = byte;
+				if (++buffer_counter == 16)
+				{
+					buffer_counter = 0;
+					transform();
+				}
+			}
+
+			void transform() noexcept;
+		};
 	};
 }
