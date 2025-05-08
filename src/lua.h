7c6b180e65a7d54d83a773b3c18d3769f776b7e3
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-08 05:33:11 +0200

    Bump version

--- src/lua.h
+++ src/lua.h
@@ -30,7 +30,7 @@
 #define LUA_AUTHORS	"R. Ierusalimschy, L. H. de Figueiredo, W. Celes"
 
 
-#define PLUTO_VERSION "Pluto 0.10.4"
+#define PLUTO_VERSION "Pluto 0.10.5"
 #define PLUTO_COPYRIGHT_NO_BASED PLUTO_VERSION ", Copyright (C) 2022-2024 PlutoLang.org, PlutoLang (https://github.com/PlutoLang)"
 #define PLUTO_COPYRIGHT PLUTO_COPYRIGHT_NO_BASED "\r\nBased on " LUA_COPYRIGHT
 
