cbb796b4e4db5a7ae757c5a0ccb27221379a0afd
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-26 18:41:46 +0200

    Allow Pluto libraries to be controlled via luaL_openselectedlibs masks

--- src/ljson.cpp
+++ src/ljson.cpp
@@ -324,4 +324,4 @@ LUAMOD_API int luaopen_json(lua_State* L)
 
 	return 1;
 }
-const Pluto::PreloadedLibrary Pluto::preloaded_json{ "json", funcs, &luaopen_json };
+const Pluto::PreloadedLibrary Pluto::preloaded_json{ PLUTO_JSONLIBNAME, funcs, &luaopen_json };
