3e0702dc6f4128197e22395faed86a493ba3eb4d
Sainan <63328889+Sainan@users.noreply.github.com>
2025-06-03 19:23:17 +0200

    Raise socket.bind error to caller, not to scheduler

--- src/lsocketlib.cpp
+++ src/lsocketlib.cpp
@@ -484,9 +484,9 @@ LUAMOD_API int luaopen_socket (lua_State *L) {
   lua_pushliteral(L, "bind");
   luaL_loadstring(L, R"EOC(
 return function(sched, port, callback)
+    local l = require"pluto:socket".listen(port)
+    assert(l, "Failed to bind port "..port)
     return sched:add(function()
-        local l = require"pluto:socket".listen(port)
-        assert(l, "Failed to bind port "..port)
         while s := l:accept() do
             sched:add(function()
                 callback(s)
