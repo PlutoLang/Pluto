fa9f453f0b94c1b99f7ad2e8bdddc79dc506c5b1
Sainan <63328889+Sainan@users.noreply.github.com>
2025-06-07 16:32:58 +0200

    Support type hinting for multiple return values

--- src/lparser.h
+++ src/lparser.h
@@ -73,7 +73,6 @@ typedef enum {
 enum ValType : lu_byte {
   VT_NONE = 0,
   VT_DUNNO,
-  VT_VOID,
   VT_NIL,
   VT_NUMBER,
   VT_INT,
@@ -88,7 +87,6 @@ enum ValType : lu_byte {
   switch (vt) {
     case VT_NONE: return "none";
     case VT_DUNNO: return "dunno";
-    case VT_VOID: return "void";
     case VT_NIL: return "nil";
     case VT_NUMBER: return "number";
     case VT_INT: return "int";
@@ -141,15 +139,18 @@ typedef struct expdesc {
 
 struct TypeHint;
 
+inline constexpr int MAX_TYPED_RETURNS = 3;
+
 struct TypeDesc {
   ValType type;
 
   /* function info */
-  Proto* proto = nullptr;
-  TypeHint* retn = nullptr;
-  static constexpr int MAX_TYPED_PARAMS = 10;
-  TypeHint* params[MAX_TYPED_PARAMS];
   bool nodiscard = false;
+  int8_t nret = 0;
+  Proto* proto = nullptr;
+  TypeHint* returns[MAX_TYPED_RETURNS];
+  static constexpr int MAX_TYPED_PARAMS = 7;
+  TypeHint* params[MAX_TYPED_PARAMS];
 
   TypeDesc() = default;
 
@@ -299,22 +300,9 @@ struct TypeHint {
     return contains(VT_NIL);
   }
 
-  void fixTypes() {
-    if (descs[1].type != VT_NONE) { /* contains more than 1 type? */
-      /* convert 'void' to 'nil' (or 'none' if we already have a 'nil') */
-      const bool already_has_nil = isNullable();
-      for (auto& desc : descs) {
-        if (desc.type == VT_VOID) {
-          desc.type = already_has_nil ? VT_NONE : VT_NIL;
-          break;
-        }
-      }
-    }
-  }
-
   [[nodiscard]] std::string toString() const {
     if (empty()) {
-      return "nil";
+      return "NOINFO";
     }
     std::string str{};
     if (isNullable()) {
