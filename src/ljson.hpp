5c7e099628285d7ef15f5cb756e942d18c08b273
Sainan <63328889+Sainan@users.noreply.github.com>
2025-09-16 23:02:12 +0200

    Use Lua code style in ljson.hpp

--- src/ljson.hpp
+++ src/ljson.hpp
@@ -1,30 +1,25 @@
 #pragma once
 
 // This function by itself is not a comprehensive check; the actual array encoding may bail to object if it detects an issue.
-inline lua_Integer isIndexBasedTable(lua_State* L, int i)
-{
-	lua_pushvalue(L, i);
-	lua_pushnil(L);
-	lua_Integer k = 1;
-	for (; lua_next(L, -2); ++k)
-	{
-		if (lua_type(L, -2) != LUA_TNUMBER)
-		{
-			lua_pop(L, 3);
-			return 0;
-		}
-		if (!lua_isinteger(L, -2))
-		{
-			lua_pop(L, 3);
-			return 0;
-		}
-		if (lua_tointeger(L, -2) <= 0)
-		{
-			lua_pop(L, 3);
-			return 0;
-		}
-		lua_pop(L, 1);
-	}
-	lua_pop(L, 1);
-	return k;
+inline lua_Integer isIndexBasedTable (lua_State *L, int i) {
+  lua_pushvalue(L, i);
+  lua_pushnil(L);
+  lua_Integer k = 1;
+  for (; lua_next(L, -2); ++k) {
+    if (lua_type(L, -2) != LUA_TNUMBER) {
+      lua_pop(L, 3);
+      return 0;
+    }
+    if (!lua_isinteger(L, -2)) {
+      lua_pop(L, 3);
+      return 0;
+    }
+    if (lua_tointeger(L, -2) <= 0) {
+      lua_pop(L, 3);
+      return 0;
+    }
+    lua_pop(L, 1);
+  }
+  lua_pop(L, 1);
+  return k;
 }
