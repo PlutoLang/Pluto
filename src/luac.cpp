9a26ad238482d8de6ed891642da4f351586b8621
Sainan <63328889+Sainan@users.noreply.github.com>
2025-12-23 12:53:54 +0100

    Revert plutoc

--- src/luac.cpp
+++ src/luac.cpp
@@ -28,7 +28,6 @@
 #include "lopcodes.h"
 #include "lopnames.h"
 #include "lstate.h"
-#include "ltable.h"
 #include "lundump.h"
 
 static void PrintFunction(const Proto* f, int full);
@@ -201,8 +200,7 @@ static int pmain(lua_State* L)
   FILE* D= (output==NULL) ? stdout : luaL_fopen(output,strlen(output),"wb",sizeof("wb")-sizeof(""));
   if (D==NULL) cannot("open");
   lua_lock(L);
-  Table *h = luaH_new(L);  /* auxiliary table used by 'luaU_dump' */
-  luaU_dump(L,f,writer,D,stripping, h);
+  luaU_dump(L,f,writer,D,stripping);
   lua_unlock(L);
   if (ferror(D)) cannot("write");
   if (fclose(D)) cannot("close");
