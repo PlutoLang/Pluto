c99d3f88e6a58f9e98c97e1287169c1045c979dd
Sainan <63328889+Sainan@users.noreply.github.com>
2025-12-27 19:46:55 +0100

    4365a45d681b4e71e3c39148489bb8eae538ccf7

--- src/lparser.h
+++ src/lparser.h
@@ -125,11 +125,12 @@ typedef struct expdesc {
     };
     struct {  /* for indexed variables */
       short idx;  /* index (R or "long" K) */
+      short vidx;  /* index in 'actvar.arr' or -1 if not a declared global */
       lu_byte t;  /* table (register or upvalue) */
     } ind;
     struct {  /* for local variables */
       lu_byte ridx;  /* register holding the variable */
-      unsigned short vidx;  /* compiler index (in 'actvar.arr')  */
+      short vidx;  /* index in 'actvar.arr' */
     } var;
   } u;
   int t;  /* patch list of 'exit when true' */
@@ -441,7 +442,7 @@ struct TypeHint {
   }
 };
 
-/* description of an active local variable */
+/* description of an active variable */
 typedef union Vardesc {
   struct {
     TValuefields;  /* constant value (if it is a compile-time constant) */
