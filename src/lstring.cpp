02e72e35a980442e710cba6a0bd568fa3fdcb095
Sainan <63328889+Sainan@users.noreply.github.com>
2025-12-28 16:55:23 +0100

    Keep luaS_hash public

--- src/lstring.cpp
+++ src/lstring.cpp
@@ -51,7 +51,7 @@ int luaS_eqstr (TString *a, TString *b) {
 }
 
 
-static unsigned luaS_hash (const char *str, size_t l, unsigned seed) {
+unsigned luaS_hash (const char *str, size_t l, unsigned seed) {
   unsigned int h = seed ^ cast_uint(l);
   for (; l > 0; l--)
     h ^= ((h<<5) + (h>>2) + cast_byte(str[l - 1]));
