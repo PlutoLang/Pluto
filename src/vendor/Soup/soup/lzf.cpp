3720f1d10333720284464da0f13cfc86980baef4
Sainan <63328889+Sainan@users.noreply.github.com>
2026-06-08 23:10:38 +0200

    Fix crypto.compress not working in WASM build

--- src/vendor/Soup/soup/lzf.cpp
+++ src/vendor/Soup/soup/lzf.cpp
@@ -3,6 +3,8 @@
 #include <cstdint>
 #include <cstring> // memset
 
+#include "alloc.hpp"
+
 /*
  * Copyright (c) 2000-2010 Marc Alexander Lehmann <schmorp@schmorp.de>
  *
@@ -56,23 +58,38 @@ NAMESPACE_SOUP
 #define LZF_USE_OFFSETS SOUP_BITS >= 64
 
 #if LZF_USE_OFFSETS
-    #define LZF_HSLOT_BIAS ((const u8 *)in_data)
+	#define LZF_HSLOT_BIAS ((const u8 *)in_data)
 	using LZF_HSLOT = unsigned int;
 #else
-    #define LZF_HSLOT_BIAS 0
+	#define LZF_HSLOT_BIAS 0
 	using LZF_HSLOT = const u8*;
 #endif
 
-    using LZF_STATE = LZF_HSLOT[HSIZE];
+	using LZF_STATE = LZF_HSLOT[HSIZE];
 
-    static constexpr auto MAX_LIT = (1 <<  5);
-    static constexpr auto MAX_OFF = (1 << 13);
-    static constexpr auto MAX_REF = ((1 << 8) + (1 << 3));
+	struct LZF_STATE_RAII
+	{
+		LZF_HSLOT* const htab;
+
+		LZF_STATE_RAII() SOUP_EXCAL
+			: htab((LZF_HSLOT*)soup::malloc(sizeof(LZF_HSLOT)* HSIZE))
+		{
+		}
+
+		~LZF_STATE_RAII() noexcept
+		{
+			soup::free(htab);
+		}
+	};
+
+	static constexpr auto MAX_LIT = (1 <<  5);
+	static constexpr auto MAX_OFF = (1 << 13);
+	static constexpr auto MAX_REF = ((1 << 8) + (1 << 3));
 
 #if __GNUC__ >= 3
-    #define expect(expr,value)         __builtin_expect ((expr),(value))
+	#define expect(expr,value)         __builtin_expect ((expr),(value))
 #else
-    #define expect(expr,value)         (expr)
+	#define expect(expr,value)         (expr)
 #endif
 
 #define expect_false(expr) expect ((expr) != 0, 0)
@@ -80,183 +97,188 @@ NAMESPACE_SOUP
 
 	unsigned int lzf::compress(const void* const in_data, unsigned int in_len, void* out_data, unsigned int out_len)
 	{
-        LZF_STATE htab;
-        const u8* ip = (const u8*)in_data;
-        u8* op = (u8*)out_data;
-        const u8* in_end = ip + in_len;
-        u8* out_end = op + out_len;
-        const u8* ref;
+#if true
+		LZF_STATE_RAII htab_raii;
+		const auto htab = htab_raii.htab;
+#else
+		LZF_STATE htab; static_assert(!SOUP_WASM); // Very big stack allocation!!!
+#endif
+		const u8* ip = (const u8*)in_data;
+		u8* op = (u8*)out_data;
+		const u8* in_end = ip + in_len;
+		u8* out_end = op + out_len;
+		const u8* ref;
 
-        /* off requires a type wide enough to hold a general pointer difference.
-         * ISO C doesn't have that (size_t might not be enough and ptrdiff_t only
-         * works for differences within a single object). We also assume that no
-         * no bit pattern traps. Since the only platform that is both non-POSIX
-         * and fails to support both assumptions is windows 64 bit, we make a
-         * special workaround for it.
-         */
+		/* off requires a type wide enough to hold a general pointer difference.
+		 * ISO C doesn't have that (size_t might not be enough and ptrdiff_t only
+		 * works for differences within a single object). We also assume that no
+		 * no bit pattern traps. Since the only platform that is both non-POSIX
+		 * and fails to support both assumptions is windows 64 bit, we make a
+		 * special workaround for it.
+		 */
 #if defined (WIN32) && defined (_M_X64)
-        unsigned _int64 off; /* workaround for missing POSIX compliance */
+		unsigned _int64 off; /* workaround for missing POSIX compliance */
 #else
-        unsigned long off;
+		unsigned long off;
 #endif
-        unsigned int hval;
-        int lit;
+		unsigned int hval;
+		int lit;
 
-        if (!in_len || !out_len)
-            return 0;
+		if (!in_len || !out_len)
+			return 0;
 
-        memset(htab, 0, sizeof(htab));
+		memset(htab, 0, sizeof(LZF_HSLOT) * HSIZE);
 
-        lit = 0; op++; /* start run */
+		lit = 0; op++; /* start run */
 
-        hval = FRST(ip);
-        while (ip < in_end - 2)
-        {
-            LZF_HSLOT* hslot;
+		hval = FRST(ip);
+		while (ip < in_end - 2)
+		{
+			LZF_HSLOT* hslot;
 
-            hval = NEXT(hval, ip);
-            hslot = htab + IDX(hval);
-            ref = *hslot + LZF_HSLOT_BIAS; *hslot = ip - LZF_HSLOT_BIAS;
+			hval = NEXT(hval, ip);
+			hslot = htab + IDX(hval);
+			ref = *hslot + LZF_HSLOT_BIAS; *hslot = ip - LZF_HSLOT_BIAS;
 
-            if (1
-                && ref < ip /* the next test will actually take care of this, but this is faster */
-                && (off = ip - ref - 1) < MAX_OFF
-                && ref > (u8*)in_data
-                && ref[2] == ip[2]
+			if (1
+				&& ref < ip /* the next test will actually take care of this, but this is faster */
+				&& (off = ip - ref - 1) < MAX_OFF
+				&& ref > (u8*)in_data
+				&& ref[2] == ip[2]
 #if STRICT_ALIGN
-                && ((ref[1] << 8) | ref[0]) == ((ip[1] << 8) | ip[0])
+				&& ((ref[1] << 8) | ref[0]) == ((ip[1] << 8) | ip[0])
 #else
-                && *(u16*)ref == *(u16*)ip
+				&& *(u16*)ref == *(u16*)ip
 #endif
-                )
-            {
-                /* match found at *ref++ */
-                unsigned int len = 2;
-                unsigned int maxlen = in_end - ip - len;
-                maxlen = maxlen > MAX_REF ? MAX_REF : maxlen;
+				)
+			{
+				/* match found at *ref++ */
+				unsigned int len = 2;
+				unsigned int maxlen = in_end - ip - len;
+				maxlen = maxlen > MAX_REF ? MAX_REF : maxlen;
 
-                if (expect_false(op + 3 + 1 >= out_end)) /* first a faster conservative test */
-                    if (op - !lit + 3 + 1 >= out_end) /* second the exact but rare test */
-                        return 0;
+				if (expect_false(op + 3 + 1 >= out_end)) /* first a faster conservative test */
+					if (op - !lit + 3 + 1 >= out_end) /* second the exact but rare test */
+						return 0;
 
-                op[-lit - 1] = lit - 1; /* stop run */
-                op -= !lit; /* undo run if length is zero */
+				op[-lit - 1] = lit - 1; /* stop run */
+				op -= !lit; /* undo run if length is zero */
 
-                for (;;)
-                {
-                    if (expect_true(maxlen > 16))
-                    {
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
+				for (;;)
+				{
+					if (expect_true(maxlen > 16))
+					{
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
 
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
 
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
 
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
-                        len++; if (ref[len] != ip[len]) break;
-                    }
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+						len++; if (ref[len] != ip[len]) break;
+					}
 
-                    do
-                        len++;
-                    while (len < maxlen && ref[len] == ip[len]);
+					do
+						len++;
+					while (len < maxlen && ref[len] == ip[len]);
 
-                    break;
-                }
+					break;
+				}
 
-                len -= 2; /* len is now #octets - 1 */
-                ip++;
+				len -= 2; /* len is now #octets - 1 */
+				ip++;
 
-                if (len < 7)
-                {
-                    *op++ = (off >> 8) + (len << 5);
-                }
-                else
-                {
-                    *op++ = (off >> 8) + (7 << 5);
-                    *op++ = len - 7;
-                }
+				if (len < 7)
+				{
+					*op++ = (off >> 8) + (len << 5);
+				}
+				else
+				{
+					*op++ = (off >> 8) + (7 << 5);
+					*op++ = len - 7;
+				}
 
-                *op++ = off;
+				*op++ = off;
 
-                lit = 0; op++; /* start run */
+				lit = 0; op++; /* start run */
 
-                ip += len + 1;
+				ip += len + 1;
 
-                if (expect_false(ip >= in_end - 2))
-                    break;
+				if (expect_false(ip >= in_end - 2))
+					break;
 
 #if ULTRA_FAST || VERY_FAST
-                --ip;
+				--ip;
 # if VERY_FAST && !ULTRA_FAST
-                --ip;
+				--ip;
 # endif
-                hval = FRST(ip);
+				hval = FRST(ip);
 
-                hval = NEXT(hval, ip);
-                htab[IDX(hval)] = ip - LZF_HSLOT_BIAS;
-                ip++;
+				hval = NEXT(hval, ip);
+				htab[IDX(hval)] = ip - LZF_HSLOT_BIAS;
+				ip++;
 
 # if VERY_FAST && !ULTRA_FAST
-                hval = NEXT(hval, ip);
-                htab[IDX(hval)] = ip - LZF_HSLOT_BIAS;
-                ip++;
+				hval = NEXT(hval, ip);
+				htab[IDX(hval)] = ip - LZF_HSLOT_BIAS;
+				ip++;
 # endif
 #else
-                ip -= len + 1;
+				ip -= len + 1;
 
-                do
-                {
-                    hval = NEXT(hval, ip);
-                    htab[IDX(hval)] = ip - LZF_HSLOT_BIAS;
-                    ip++;
-                } while (len--);
+				do
+				{
+					hval = NEXT(hval, ip);
+					htab[IDX(hval)] = ip - LZF_HSLOT_BIAS;
+					ip++;
+				} while (len--);
 #endif
-            }
-            else
-            {
-                /* one more literal byte we must copy */
-                if (expect_false(op >= out_end))
-                    return 0;
+			}
+			else
+			{
+				/* one more literal byte we must copy */
+				if (expect_false(op >= out_end))
+					return 0;
 
-                lit++; *op++ = *ip++;
+				lit++; *op++ = *ip++;
 
-                if (expect_false(lit == MAX_LIT))
-                {
-                    op[-lit - 1] = lit - 1; /* stop run */
-                    lit = 0; op++; /* start run */
-                }
-            }
-        }
+				if (expect_false(lit == MAX_LIT))
+				{
+					op[-lit - 1] = lit - 1; /* stop run */
+					lit = 0; op++; /* start run */
+				}
+			}
+		}
 
-        if (op + 3 > out_end) /* at most 3 bytes can be missing here */
-            return 0;
+		if (op + 3 > out_end) /* at most 3 bytes can be missing here */
+			return 0;
 
-        while (ip < in_end)
-        {
-            lit++; *op++ = *ip++;
+		while (ip < in_end)
+		{
+			lit++; *op++ = *ip++;
 
-            if (expect_false(lit == MAX_LIT))
-            {
-                op[-lit - 1] = lit - 1; /* stop run */
-                lit = 0; op++; /* start run */
-            }
-        }
+			if (expect_false(lit == MAX_LIT))
+			{
+				op[-lit - 1] = lit - 1; /* stop run */
+				lit = 0; op++; /* start run */
+			}
+		}
 
-        op[-lit - 1] = lit - 1; /* end run */
-        op -= !lit; /* undo run if length is zero */
+		op[-lit - 1] = lit - 1; /* end run */
+		op -= !lit; /* undo run if length is zero */
 
-        return op - (u8*)out_data;
+		return op - (u8*)out_data;
 	}
 
 #undef FRST
