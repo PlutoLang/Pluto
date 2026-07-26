5ea7b9bb1d289f7408e772816ad1fbfc83e45dc9
Sainan <63328889+Sainan@users.noreply.github.com>
2026-07-26 19:10:13 +0200

    0465c23b3ee214ea3a117ab9d69a83cf85e7a82f

--- src/lcode.cpp
+++ src/lcode.cpp
@@ -2149,8 +2149,6 @@ void luaK_finish (FuncState *fs) {
     p->flag &= cast_byte(~PF_VAHID);  /* then it will not use hidden args. */
   for (i = 0; i < fs->pc; i++) {
     Instruction *pc = &p->code[i];
-    /* avoid "not used" warnings when assert is off (for 'onelua.c') */
-    (void)luaP_isOT; (void)luaP_isIT;
     lua_assert(i == 0 || luaP_isOT(*(pc - 1)) == luaP_isIT(*pc));
     switch (GET_OPCODE(*pc)) {
       case OP_RETURN0: case OP_RETURN1: {
