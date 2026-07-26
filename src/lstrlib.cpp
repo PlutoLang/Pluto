d18b94396391cd73e11c1ce1937dacd876874020
Sainan <63328889+Sainan@users.noreply.github.com>
2026-07-26 18:48:46 +0200

    efddc2309c5ff8a1842bea8a9c0d7d4a5d6e1e60

--- src/lstrlib.cpp
+++ src/lstrlib.cpp
@@ -811,19 +811,25 @@ static int nospecials (const char *p, size_t l) {
 }
 
 
+/*
+** Prepare state for matches. These fields are not affected by each match.
+*/
 static void prepstate (MatchState *ms, lua_State *L,
                        const char *s, size_t ls, const char *p, size_t lp) {
   ms->L = L;
-  ms->matchdepth = MAXCCALLS;
   ms->src_init = s;
   ms->src_end = s + ls;
   ms->p_end = p + lp;
 }
 
 
+/*
+** (Re)prepare state for a match, setting fields that change during
+** each match.
+*/
 static void reprepstate (MatchState *ms) {
+  ms->matchdepth = MAXCCALLS;
   ms->level = 0;
-  lua_assert(ms->matchdepth == MAXCCALLS);
 }
 
 
